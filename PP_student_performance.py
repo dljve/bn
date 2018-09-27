9ce54989b61d78c2ab33645683dbafff7c9bf2e5
MJJdG <35668031+MJJdG@users.noreply.github.com>
2018-09-27 12:52:20 +0200

    Add files via upload

--- PP_student_performance.py
+++ PP_student_performance.py
@@ -7,13 +7,16 @@ Created on Thu Sep 27 11:41:58 2018
 """
 import pandas as pd
 
-#Import datasets
+#Import datasets, add subjects variable
 mat = pd.read_csv('student-mat.csv',sep=";")
+mat['subject'] = ["M" for x in range(mat.shape[0])]
 por = pd.read_csv('student-por.csv',sep=";")
+por['subject'] = ["P" for x in range(por.shape[0])]
 
-#Merge subjects
-matpor = pd.merge(mat, por, on=["school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"])
-matpor.to_csv('matpor.csv', sep=',')
+#Merge/concat subjects
+#matpor = pd.merge(mat, por, on=["school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"], suffixes=('_mat', '_por'))
+matpor = pd.concat([mat,por])
+matpor.to_csv('matpor_concat.csv', sep=',')
 
 #Bin into categories
 # not necessary for our dataset
